365255998d28d2c7b486abdc9d02a86f826bab3c
Morty MCFLY <77272753+Morty-debug@users.noreply.github.com>
2022-03-24 15:38:16 -0600

    Update Avanzado.go

--- Avanzado.go
+++ Avanzado.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"fmt"
+	"bytes"
 )
 
 type Response struct {
@@ -20,6 +21,8 @@ type Matchs struct {
 func main() {
 	http.HandleFunc("/", writejson)
 	http.HandleFunc("/leer", readjson)
+	http.HandleFunc("/recibir", inputjson)
+	http.HandleFunc("/enviar", sendjson)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -30,7 +33,8 @@ func writejson(w http.ResponseWriter, r *http.Request) {
 	Estructura := Response {"Identificado",Matches}
 	js, err := json.Marshal(Estructura)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, "{\"Estado\":\"No se logro crear JSON\",\"Comparaciones\":null}") 
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
@@ -43,11 +47,15 @@ func readjson(w http.ResponseWriter, r *http.Request) {
 	url := "http://localhost:8080/"
 	res, err := http.Get(url)
 	if err != nil {
-		panic(err.Error())
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, "{\"Estado\":\"No se logro leer JSON de la URL "+url+"\",\"Comparaciones\":null}") 
+		return
 	}
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		panic(err.Error())
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, "{\"Estado\":\"No se logro leer JSON de la URL "+url+"\",\"Comparaciones\":null}") 
+		return
 	}
 	var data Response
 	json.Unmarshal(body, &data)
@@ -59,3 +67,53 @@ func readjson(w http.ResponseWriter, r *http.Request) {
 	}
 	return
 }
+
+func inputjson(w http.ResponseWriter, r *http.Request) {
+	headerContentTtype := r.Header.Get("Content-Type")
+	if headerContentTtype != "application/json" {
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, "{\"Estado\":\"Formato no compatible\",\"Comparaciones\":null}") 
+		return
+	}
+
+	var data Response
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+	err := decoder.Decode(&data)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, "{\"Estado\":\"Estructura no compatible\",\"Comparaciones\":null}") 
+		return
+	}
+	
+	fmt.Printf("Estado: %s\n", data.Estado)
+	fmt.Fprintf(w, "Estado: %s\n", data.Estado) 
+	for i:=0; i<len(data.Comparaciones); i++{
+		fmt.Printf("Id: %s Puntuacion: %d\n", data.Comparaciones[i].Id, data.Comparaciones[i].Puntuacion)
+		fmt.Fprintf(w, "Id: %s Puntuacion: %d\n", data.Comparaciones[i].Id, data.Comparaciones[i].Puntuacion) 
+	}
+	return
+}
+
+func sendjson(w http.ResponseWriter, r *http.Request) {
+	url := "http://localhost:8080/recibir"
+    
+    var jsonStr = []byte(`{"Estado": "Identificado","Comparaciones": [{"Id": "DGME-1","Puntuacion": 100},{"Id": "DGME-2","Puntuacion": 5}]}`)
+    req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+    req.Header.Set("X-Custom-Header", "myvalue")
+    req.Header.Set("Content-Type", "application/json")
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+
+    fmt.Fprintf(w, "Respuesta Estado: %s\n", resp.Status) 
+    fmt.Fprintf(w, "Respuesta Encabezado: %s\n", resp.Header) 
+    body, _ := ioutil.ReadAll(resp.Body)
+    fmt.Fprintf(w, "Respuesta Cuerpo: \n\n%s\n", string(body)) 
+    return
+}
+
